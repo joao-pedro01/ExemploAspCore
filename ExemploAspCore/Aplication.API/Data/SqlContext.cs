e45d29e0d50af8e826a5771dd7d763668f50ac68
joao-pedro01 <joao_pedro01@terra.com.br>
2022-05-19 22:21:45 -0300

    Update and create contatos

---
+++ ExemploAspCore/Aplication.API/Data/SqlContext.cs
@@ -0,0 +1,24 @@
+﻿using Aplication.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplication.API.Data
+{
+    public class SqlContext : DbContext
+    {
+        public DbSet<Contatos> Contato { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
